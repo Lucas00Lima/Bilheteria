3853dec91556adebd44774eb5c24ee2585918145
Lucas00Lima <117579221+Lucas00Lima@users.noreply.github.com>
2023-01-16 19:13:06 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function Sortear(){
+    let n = Number(document.getElementById('nun1').value)
+    let s = 7
+        if ( n < 10 ){
+            if(n < 7)
+                res.innerHTML = ('Seu valor está mais acima')
+            else{
+                res.innerHTML = ('Seu valor está mais para baixo') 
+                }
+            }
+            else {
+                alert('Insira um numero entre 1 e 10')
+            }
+        if(n==7){
+            alert('Você ganhou')}
+            res.innerHTML = ('Você GANHOU')
+}
